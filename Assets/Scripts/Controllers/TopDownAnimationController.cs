dcee1904ebf31508124bb82723c5026b431dacf0
shss1105 <kimseung2331@naver.com>
2024-01-22 17:22:37 +0900

    Animation
    캐릭터 애니메이션 추가
    Idle, Run

---
+++ Assets/Scripts/Controllers/TopDownAnimationController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownAnimationController : TopDownAnimations
+{
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 obj)
+    {
+        animator.SetBool(IsWalking, obj.magnitude > .5f);
+    }
+
+}
