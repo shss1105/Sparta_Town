b3d319179cf317913bebc923ba7e58a7a9f318fa
shss1105 <kimseung2331@naver.com>
2024-01-22 14:39:45 +0900

    Move
    Input Action사용
    
    키보드 A/W/S/D로 이동기능 추가

--- Assets/Scripts/Controllers/PlayerInputController.cs
+++ Assets/Scripts/Controllers/PlayerInputController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerInputController : MonoBehaviour
+public class PlayerInputController : TopDownCharacterController
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera _camera;
+    private void Awake()
     {
-        
+        _camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputValue value)
     {
-        
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        if (newAim.magnitude >= .9f)
+        {
+            CallLookEvent(newAim);
+        }
+    }
+
+    public void OnFire(InputValue value)
+    {
+        Debug.Log("OnFire" + value.ToString());
     }
 }
--- Assets/Scripts/Entities/TopDownMovement.cs
+++ Assets/Scripts/Entities/TopDownMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class TopDownMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TopDownCharacterController _controller;
+
+    private Vector2 _movementDirection = Vector2.zero;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
     {
-        
+        _controller = GetComponent<TopDownCharacterController>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovment(_movementDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _movementDirection = direction;
+    }
+
+    private void ApplyMovment(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        _rigidbody.velocity = direction;
     }
 }
