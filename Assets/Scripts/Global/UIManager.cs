2e866f7ff36004ce57fd9e0ba5623a1863b2fa80
shss1105 <kimseung2331@naver.com>
2024-01-23 16:03:23 +0900

    Time
    현재시간 표시

---
+++ Assets/Scripts/Global/UIManager.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] Text time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time.text = DateTime.Now.ToString("HH : mm");
+    }
+}
